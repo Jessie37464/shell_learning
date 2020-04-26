925d0c1f3eeb00998294818fc49e95af55f03d5a
zhengqixin <374649328>
2020-04-26 20:51:29 +0800

    first

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+#echo "hello world!"
+
+#变量
+#your_name="zqx"
+#echo $your_name
+
+#双引号+变量
+#your_name='zqx'
+#output="i know you are $your_name!"
+#echo $output
+
+
+#传入参数
+#echo "Shell 传递参数实例！";
+#echo "执行的文件名：$0";
+#echo "第一个参数为：$1";
+#echo "第二个参数为：$2";
+#echo "第三个参数为：$3";
+
+
+#特殊字符
+echo "Shell 传递参数实例！";
+echo "第一个参数为：$1";
+
+echo "参数个数为：$#";
+echo "传递的参数作为一个字符串显示：$*";
